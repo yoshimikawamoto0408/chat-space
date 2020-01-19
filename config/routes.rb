075980d43134a7f8702b9ebe0e842333799a62a9
yoshimikawamoto0408 <58136158+yoshimikawamoto0408@users.noreply.github.com>
2020-01-19 20:00:11 +0900

    元に戻った

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'messages#index'
+ resources :users, only: [:edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20200105122334) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
